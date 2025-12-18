a5e2afaa9a65f1686a930ea99b9b997746d60bd1
Prothes Barai <aspprothesbarai1619@gmail.com>
2025-12-18 17:15:28 +0600

    asp bb

--- lib/screen/bottom_navigator_items_screens/chat_screens/chat_screen.dart
+++ lib/screen/bottom_navigator_items_screens/chat_screens/chat_screen.dart
@@ -1,7 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import '../../group_chats/chat_room/ChatRoom.dart';
+import 'ChatRoom.dart';
 
 
 class ChatScreen extends StatefulWidget {
