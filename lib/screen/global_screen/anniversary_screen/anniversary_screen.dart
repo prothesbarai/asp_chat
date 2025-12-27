d4b8e81f067028450cc20a2e13b1e121decb3eac
Prothes Barai <aspprothesbarai1619@gmail.com>
2025-12-27 11:27:31 +0600

    asp v55

--- lib/screen/global_screen/anniversary_screen/anniversary_screen.dart
+++ lib/screen/global_screen/anniversary_screen/anniversary_screen.dart
@@ -1,9 +1,13 @@
+import 'dart:io';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:loading_animation_widget/loading_animation_widget.dart';
 import 'package:lottie/lottie.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:pinput/pinput.dart';
+import 'package:screenshot/screenshot.dart';
+import 'package:share_plus/share_plus.dart';
 import '../../../utils/constant/app_colors.dart';
 import 'anniversary_date_helper.dart';
 
@@ -21,7 +25,11 @@ class _AnniversaryScreenState extends State<AnniversaryScreen> {
   String _anniversaryTitle = "";
   String _anniversaryYears = "";
   bool isLoading = false;
+  bool _isScreenshotMode = false;
   final TextEditingController _pinController = TextEditingController();
+  final ScreenshotController _screenshotController = ScreenshotController();
+  String _shareText = "";
+
 
 
 
@@ -48,6 +56,51 @@ class _AnniversaryScreenState extends State<AnniversaryScreen> {
   /// <<< Get Data From Firebase ===============================================
 
 
+  /// >>> Send Wishes Button Functional ========================================
+  void _showEditTextDialog() {
+    final TextEditingController textController = TextEditingController(text: _shareText);
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          backgroundColor: Theme.of(context).colorScheme.surface, // explicitly background
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+          title: Text("Edit Your Wishes 💖", style: TextStyle(color: Theme.of(context).colorScheme.onSurface, fontWeight: FontWeight.bold,),),
+          content: TextField(controller: textController, style: TextStyle(color: Theme.of(context).colorScheme.onSurface), minLines: 1, maxLines: null, decoration: InputDecoration(hintText: "Write something beautiful...", border: OutlineInputBorder(), filled: true, fillColor: Theme.of(context).colorScheme.surface,),),
+          actions: [
+            ElevatedButton(onPressed: () => Navigator.pop(context), child: const Text("Cancel"),),
+            ElevatedButton(onPressed: () {_shareText = textController.text;Navigator.pop(context);_shareWishes();}, child: const Text("Share"),),
+          ],
+        );
+      },
+    );
+
+  }
+  Future<void> _shareWishes() async {
+    try {
+      setState(() { _isScreenshotMode = true; }); // Screenshot mode on
+      await Future.delayed(const Duration(milliseconds: 50)); // Ensure UI update
+      final image = await _screenshotController.capture(delay: const Duration(milliseconds: 300));
+      if (image == null) return;
+
+      final directory = await getTemporaryDirectory();
+      final imageFile = File('${directory.path}/anniversary.png');
+      await imageFile.writeAsBytes(image);
+
+      final params = ShareParams(
+        text: "💖 Happy Anniversary 💖\n\n$_anniversaryTitle\n$_anniversaryYears\n\n$_anniversaryMessage",
+        files: [XFile(imageFile.path)],
+      );
+      await SharePlus.instance.share(params);
+    } catch (e) {
+      debugPrint("Share error: $e");
+    } finally {
+      setState(() { _isScreenshotMode = false; }); // Screenshot mode off
+    }
+  }
+  /// <<< Send Wishes Button Functional ========================================
+
+
   @override
   void dispose() {
     _pinController.dispose();
@@ -57,7 +110,6 @@ class _AnniversaryScreenState extends State<AnniversaryScreen> {
   @override
   Widget build(BuildContext context) {
     final data = sequentialAnniversaryHelper();
-
     return Scaffold(
       body: Stack(
         children: [
@@ -70,40 +122,55 @@ class _AnniversaryScreenState extends State<AnniversaryScreen> {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  // >>> Lottie Animation
-                  Lottie.asset('assets/lottie/bird.json', width: double.infinity, height: 200,),
-                  const SizedBox(height: 20),
-                  // >>> Anniversary Date Card
-                  Container(
-                    padding: const EdgeInsets.symmetric(horizontal: 18, vertical: 16),
-                    decoration: BoxDecoration(borderRadius: BorderRadius.circular(24), gradient: LinearGradient(colors: data["isToday"] ? [Colors.pinkAccent, Colors.purple] : [Colors.white.withValues(alpha : 0.25), Colors.white.withValues(alpha : 0.08),],), border: Border.all(color: Colors.white24),),
-                    child: Row(
-                      children: [
-                        Icon(data["icon"], color: Colors.white, size: 24),
-                        const SizedBox(width: 12),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(data["title"], style: GoogleFonts.lato(fontSize: 16, fontWeight: FontWeight.bold, color: Colors.white,),),
-                            const SizedBox(height: 4),
-                            Text("${data["date"]} • ${data["status"]}", style: GoogleFonts.lato(fontSize: 14, color: Colors.white70,),),
+
+                  Screenshot(
+                    controller: _screenshotController,
+                    child: Container(
+                      padding: _isScreenshotMode ? const EdgeInsets.symmetric(horizontal: 20, vertical: 20) : null,
+                      decoration: _isScreenshotMode ? BoxDecoration(gradient: LinearGradient(colors: [Colors.pink.shade400, Colors.purple.shade900], begin: Alignment.topLeft, end: Alignment.bottomRight,),) : null,
+                      child: Column(
+                        children: [
+                          // >>> Lottie Animation
+                          Lottie.asset('assets/lottie/bird.json', width: double.infinity, height: 200,),
+                          const SizedBox(height: 20),
+                          // >>> Anniversary Date Card
+                          Container(
+                            padding: const EdgeInsets.symmetric(horizontal: 18, vertical: 16),
+                            decoration: BoxDecoration(borderRadius: BorderRadius.circular(24), gradient: LinearGradient(colors: data["isToday"] ? [Colors.pinkAccent, Colors.purple] : [Colors.white.withValues(alpha : 0.25), Colors.white.withValues(alpha : 0.08),],), border: Border.all(color: Colors.white24),),
+                            child: Row(
+                              children: [
+                                Icon(data["icon"], color: Colors.white, size: 24),
+                                const SizedBox(width: 12),
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(data["title"], style: GoogleFonts.lato(fontSize: 16, fontWeight: FontWeight.bold, color: Colors.white,),),
+                                    const SizedBox(height: 4),
+                                    Text("${data["date"]} • ${data["status"]}", style: GoogleFonts.lato(fontSize: 14, color: Colors.white70,),),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                          const SizedBox(height: 20),
+                          if(data['title'] != "Upcoming Anniversary")...[
+                            // >>> Title
+                            Text('Happy $_anniversaryTitle', textAlign: TextAlign.center, style: GoogleFonts.pacifico(fontSize: 36, color: Colors.white, fontWeight: FontWeight.bold,),),
+                            const SizedBox(height: 10),
+                            // >>> Subtitle
+                            Text(_anniversaryYears, textAlign: TextAlign.center, style: GoogleFonts.lato(fontSize: 22, color: Colors.white70,),),
+                            const SizedBox(height: 30),
+                            SizedBox(
+                              child: isLoading ? LoadingAnimationWidget.staggeredDotsWave(color: AppColors.primaryColor, size: 50,) :
+                              Text(_anniversaryMessage,textAlign: TextAlign.center, style: GoogleFonts.lato(fontSize: 22, color: Colors.white70,),),
+                            ),
                           ],
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
                   ),
-                  const SizedBox(height: 20),
+
                   if(data['title'] != "Upcoming Anniversary")...[
-                    // >>> Title
-                    Text('Happy $_anniversaryTitle', textAlign: TextAlign.center, style: GoogleFonts.pacifico(fontSize: 36, color: Colors.white, fontWeight: FontWeight.bold,),),
-                    const SizedBox(height: 10),
-                    // >>> Subtitle
-                    Text(_anniversaryYears, textAlign: TextAlign.center, style: GoogleFonts.lato(fontSize: 22, color: Colors.white70,),),
-                    const SizedBox(height: 30),
-                    SizedBox(
-                      child: isLoading ? LoadingAnimationWidget.staggeredDotsWave(color: AppColors.primaryColor, size: 50,) :
-                      Text(_anniversaryMessage,textAlign: TextAlign.center, style: GoogleFonts.lato(fontSize: 22, color: Colors.white70,),),
-                    ),
                     const SizedBox(height: 30),
                     // >>> History Content (shows only if password is correct)
                     if (_showHistory) _buildHistoryContent(),
@@ -131,16 +198,17 @@ class _AnniversaryScreenState extends State<AnniversaryScreen> {
                           flex: 1,
                           child: ElevatedButton(
                             style: ElevatedButton.styleFrom(padding: const EdgeInsets.symmetric(horizontal: 0, vertical: 14), shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30),), backgroundColor: Colors.pinkAccent,),
-                            onPressed: () {},
+                            onPressed: () {
+                              _shareText =
+                              "💖 Happy Anniversary 💖\n\n$_anniversaryTitle\n$_anniversaryYears\n\n$_anniversaryMessage";
+                              _showEditTextDialog();
+                            },
                             child: const Text('Send Wishes', style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),),
                           ),
                         ),
                       ],
                     ),
                   ],
-
-
-
                   const SizedBox(height: 20),
 
                 ],
