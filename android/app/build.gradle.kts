d01826ac744f1ef78a4387cf519af7cc0afbcdea
Prothes Barai <aspprothesbarai1619@gmail.com>
2026-01-21 11:09:50 +0600

    asp v444

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,11 +9,11 @@ plugins {
 }
 
 
-/*val keystoreProperties = Properties()
+val keystoreProperties = Properties()
 val keystorePropertiesFile = rootProject.file("key.properties")
 if (keystorePropertiesFile.exists()) {
     keystoreProperties.load(FileInputStream(keystorePropertiesFile))
-}*/
+}
 
 android {
     namespace = "com.asp.asp_chat"
@@ -47,20 +47,20 @@ android {
 
 
 
-/*    signingConfigs {
+    signingConfigs {
         create("release") {
             storeFile = file(keystoreProperties["storeFile"] as String)
             storePassword = keystoreProperties["storePassword"] as String
             keyAlias = keystoreProperties["keyAlias"] as String
             keyPassword = keystoreProperties["keyPassword"] as String
         }
-    }*/
+    }
 
     buildTypes {
         getByName("release") {
             isMinifyEnabled = true
             isShrinkResources = true
-            /*signingConfig = signingConfigs.getByName("release")*/
+            signingConfig = signingConfigs.getByName("release")
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
         getByName("debug") {
